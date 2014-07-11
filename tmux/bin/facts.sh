81a1a908d2794fac992aebfa631691180b6b3814
Bennett Rogers <bennett@crocodoc.com>
2014-07-11 21:52:51 +0000

    tmux status line shows facter info

--- tmux/bin/facts.sh
+++ tmux/bin/facts.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-if [[ -e /etc/node.facts ]]; then
-    CLUSTER=`sed -n -e 's/^cluster=//p' /etc/node.facts`
-    TIER=`sed -n -e 's/^tier=//p' /etc/node.facts`
-    echo $CLUSTER $TIER
-fi
+ENVIRONMENT=$(facter environment)
+ROLE=$(facter node_role)
+echo $ENVIRONMENT $ROLE

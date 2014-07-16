3d8f189c6de36cea917acb3b92c3b9d1d2f9f54e
Bennett Rogers <bennett@crocodoc.com>
2014-07-16 05:02:45 +0000

    adding some subtree help files

--- subtree_add.sh
+++ subtree_add.sh
@@ -9,26 +9,36 @@ usage: $0 [options]
 
 OPTIONS:
    -h      Show this message
+   -n      Name of the remote/branch/subtree to create
    -r      URL of the remote to create a subtree of
    -o      Output path for the resulting subtree
+   -b      Branch in which to install the subtree
 EOF
 }
 
 ### Parse parameters
+NAME=""
 URL=""
 OUT_PATH=""
-while getopts ?h:r:o:? opt
+BRANCH="master"
+while getopts ?hn:r:o:b:? opt
 do
     case $opt in
         h)
             usage
             exit 1
             ;;
+        n)
+            NAME="$OPTARG"
+            ;;
         r)
             URL="$OPTARG"
             ;;
         o)
             OUT_PATH="$OPTARG"
+            ;;
+        b)
+            BRANCH="$OPTARG"
             ;;
         ?)
             usage
@@ -37,16 +47,16 @@ do
     esac
 done
 
-if [[ -z $URL ]] || [[ -z $OUT_PATH ]]; then
+if [[ -z $NAME ]] || [[ -z $URL ]] || [[ -z $OUT_PATH ]] || [[ -z $BRANCH ]]; then
     usage
     exit 1
 fi
 
-NAME=${URL##*/}
-
+git stash
 git remote add subtree_$NAME $URL
 git fetch subtree_$NAME
 git checkout -b subtree_${NAME} subtree_$NAME/master
-git checkout master
+git checkout $BRANCH
 git read-tree --prefix=$OUT_PATH -u subtree_${NAME}
 git commit -m "[subtree] adding $NAME"
+git stash pop

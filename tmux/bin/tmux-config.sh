b73a9ea8f38c7d59f6476728f557badad556bab1
Bennett Rogers <bennettrogers@gmail.com>
2017-01-02 15:33:12 -0800

    making tmux status line responsive

---
+++ tmux/bin/tmux-config.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+WIDTH=${1}
+
+SMALL=80
+MEDIUM=140
+
+DATETIME="#[fg=colour243,bg=colour237,nobold,nounderscore]#[fg=colour15,bg=colour243] $(date +'%Y-%m-%d')  $(date +'%H:%M')"
+
+if [ "$WIDTH" -ge "$SMALL" ]; then
+    HOST="#[fg=colour68,bg=colour243,nobold,nounderscore,italics]#[fg=colour15,bg=colour68] #h"
+fi
+
+# Spit out the final string, and collapse spaces
+echo "$DATETIME $HOST " | sed 's/ *$/ /g'
